9c0e063e584b6c060081dd2c9bb270bcf6c6249d
diego boy <diegolive5000@gmail.com>
2022-02-04 17:16:16 +0100

    add goal model, routes, actions and views // and die inside a little bit

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,49 @@
+class GoalsController < ApplicationController
+  before_action :set_goal, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @goals = Goal.all
+  end
+
+  def show
+  end
+
+  def new
+    @goal = Goal.new
+  end
+  
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to goals_path, notice: "You set up your next goal!"
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @goal.update(goal_params)
+      redirect_to goal_path, notice: "Goal updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @goal.destroy
+    redirect_to goals_path, notice: "Your Goal was deleted"
+  end
+
+  private
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+
+  def goal_params
+    params.require(:goal).permit(:id, :title, :completed)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_230430) do
+ActiveRecord::Schema.define(version: 2022_02_04_103349) do
+
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
