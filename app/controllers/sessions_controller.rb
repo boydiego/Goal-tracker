bbb298b13ccb6fa1a9cc39009389f15da06cc321
diego boy <diegolive5000@gmail.com>
2022-02-04 02:06:46 +0100

    handle session cookies, log in and log out users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Welcome back #{user.name}"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You logged out. See you soon 👋"
+  end
+end
