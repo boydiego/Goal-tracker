7350fb920d9152895c684502dcbcfbfdb82ffb8d
diego boy <diegolive5000@gmail.com>
2022-02-06 00:04:28 +0100

    create associations and update schema, fix styling of vision board imgs, new font

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,7 +2,7 @@ class GoalsController < ApplicationController
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
   def index
-    @goals = Goal.all
+    @goals = current_user.goals
   end
 
   def show
@@ -14,6 +14,7 @@ class GoalsController < ApplicationController
   
   def create
     @goal = Goal.new(goal_params)
+    @goal.user = current_user
     if @goal.save
       redirect_to goals_path, notice: "You set up your next goal!"
     else
@@ -39,11 +40,16 @@ class GoalsController < ApplicationController
 
   private
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
   def set_goal
     @goal = Goal.find(params[:id])
   end
 
   def goal_params
-    params.require(:goal).permit(:id, :title, :image, :completed)
+    params.require(:goal).permit(:id, :title, :image, :completed, :user_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_190837) do
+ActiveRecord::Schema.define(version: 2022_02_05_205708) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2022_02_04_190837) do
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema.define(version: 2022_02_04_190837) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "goals", "users"
 end
