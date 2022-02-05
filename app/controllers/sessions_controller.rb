fdd46ff6256fb57e1a62645a99f7baa07aaa68c5
diego boy <diegolive5000@gmail.com>
2022-02-05 18:36:48 +0100

    visionboard hover effect on goals, font size adjustments, form shadows fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Welcome back #{user.name}"
+      redirect_to goals_path, notice: "Welcome back #{user.name}"
     else
       flash[:alert] = "Invalid email or password"
       render :new
