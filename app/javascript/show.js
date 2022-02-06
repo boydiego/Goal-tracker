81a2fd61fe1a31fa9a62b8dd3b234adfcb66c099
diego boy <diegolive5000@gmail.com>
2022-02-06 04:52:57 +0100

    add styling to steps on show page

--- app/javascript/show.js
+++ app/javascript/show.js
@@ -2,13 +2,20 @@ function init() {
 
   const titleContainer = document.querySelector('.title-container');
   const showSettings = document.querySelector('.show-settings');
+  const stepMarkers = document.querySelectorAll('.step-marker');
 
   titleContainer.addEventListener('mouseover', function() {
     showSettings.classList.toggle('active');
   });
 
   titleContainer.addEventListener('click', function() {
-    titleContainer.classList.toggle('done')
+    titleContainer.classList.toggle('done');
+  })
+
+  stepMarkers.forEach(function(stepMarker) {
+    stepMarker.addEventListener('click', function() {
+      stepMarker.classList.toogle('done');
+    })
   })
 
 }
